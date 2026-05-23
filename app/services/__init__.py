45d5c8391beed9ecc57d7415f99580807c494f86
Fradj-Aissa <fraais1920@gmail.com>
2026-05-23 20:13:01 +0100

    fix: remove eager import of validate_pdf_upload to enable lazy loading and prevent libmagic import error on startup

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,2 +1,3 @@
-from app.services.file_validator import validate_pdf_upload
 from app.services.storage_service import BaseStorage, LocalStorage, S3Storage, get_storage
+
+__all__ = ["BaseStorage", "LocalStorage", "S3Storage", "get_storage"]
